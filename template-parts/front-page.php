6fe8ce7fd9fe6894db0452b984e4c71fb99bade6
ko-nagai-0801 <ko.nagai.0801@gmail.com>
2025-06-09 14:10:32 +0900

    ＴＥＬアイコン追加

--- template-parts/front-page.php
+++ template-parts/front-page.php
@@ -168,7 +168,7 @@ get_header();
   <section id="contact" class="cta section">
     <div class="container text-center">
       <h2 class="cta__title"><?php esc_html_e( 'お問い合わせ', 'sanai-textdomain' ); ?></h2>
-      <p class="cta__tel"><a href="tel:0459512722">045-951-2722</a></p>
+      <p class="cta__tel"><a href="tel:0459512722"><i class="bi bi-telephone-fill"></i> 045-951-2722</a></p>
       <a href="<?php echo esc_url( home_url( '/contact/' ) ); ?>" class="btn btn--cta"><?php esc_html_e( 'お問い合わせはこちら', 'sanai-textdomain' ); ?></a>
     </div>
   </section>
