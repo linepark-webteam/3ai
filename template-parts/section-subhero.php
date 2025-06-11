2ece17ae4cb07eb1aa45fd90ede6e6089a383ffc
ko-nagai-0801 <ko.nagai.0801@gmail.com>
2025-06-11 13:44:19 +0900

    Update: subheroの画像について、管理画面からアイキャッチ画像が設定された場合はその画像に変更するようテンプレートを修正

--- template-parts/section-subhero.php
+++ template-parts/section-subhero.php
@@ -3,12 +3,20 @@
  * template-parts/section-subhero.php
  * Subhero セクション用テンプレートパーツ
  */
-?>
-<section id="subhero" class="subhero">
-  <!-- オーバーレイ -->
-  <div class="subhero__overlay"></div>
 
-  <!-- コンテナ -->
+// アイキャッチ画像があればその URL、なければ共通の背景画像を指定
+if ( has_post_thumbnail() ) {
+    $bg_url = get_the_post_thumbnail_url( get_the_ID(), 'full' );
+} else {
+    $bg_url = get_template_directory_uri() . '/assets/img/subhero-common-bg.jpg';
+}
+?>
+<section
+  id="subhero"
+  class="subhero"
+  style="background-image: url('<?php echo esc_url( $bg_url ); ?>');"
+>
+  <div class="subhero__overlay"></div>
   <div class="subhero__inner container text-center">
     <h1 class="subhero__title"><?php the_title(); ?></h1>
   </div>
