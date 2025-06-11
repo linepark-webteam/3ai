5a4d8c5d97672ff3bc92b994cc9d0877af9c231b
ko-nagai-0801 <ko.nagai.0801@gmail.com>
2025-06-11 14:09:25 +0900

    Update: subhero.phpをアップデート。アイキャッチがあった場合にその画像を表示し、無ければCSSで指定した画像になるように。

--- template-parts/section-subhero.php
+++ template-parts/section-subhero.php
@@ -4,18 +4,20 @@
  * Subhero セクション用テンプレートパーツ
  */
 
-// アイキャッチ画像があればその URL、なければ共通の背景画像を指定
+// デフォルトの背景は CSS で設定しておく
+// CSS: .subhero { background: url("../img/subhero-common-bg.jpg") center/cover no-repeat; }
+
+$inline_style = '';
 if ( has_post_thumbnail() ) {
+    // アイキャッチがあれば、その URL を取得
     $bg_url = get_the_post_thumbnail_url( get_the_ID(), 'full' );
-} else {
-    $bg_url = get_template_directory_uri() . '/assets/img/subhero-common-bg.jpg';
+    $inline_style = sprintf(
+        'style="background-image: url(%s);"',
+        esc_url( $bg_url )
+    );
 }
 ?>
-<section
-  id="subhero"
-  class="subhero"
-  style="background-image: url('<?php echo esc_url( $bg_url ); ?>');"
->
+<section id="subhero" class="subhero" <?php echo $inline_style; ?>>
   <div class="subhero__overlay"></div>
   <div class="subhero__inner container text-center">
     <h1 class="subhero__title"><?php the_title(); ?></h1>
