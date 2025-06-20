523b3e6177671e36e93156a8af3894314fc48d1a
ko-nagai-0801 <ko.nagai.0801@gmail.com>
2025-06-20 10:38:32 +0900

    Update: 会社情報テーブルを更新

--- template-parts/section-company-info.php
+++ template-parts/section-company-info.php
@@ -15,30 +15,48 @@
   <table class="company-info__table">
     <tbody>
       <tr>
-        <th><?php esc_html_e( '会社名', 'sanai-textdomain' ); ?></th>
-        <td><?php esc_html_e( '三愛不動産管理', 'sanai-textdomain' ); ?></td>
+        <th><?php esc_html_e( '社名', 'sanai-textdomain' ); ?></th>
+        <td><?php esc_html_e( '株式会社三愛不動産管理', 'sanai-textdomain' ); ?></td>
       </tr>
       <tr>
         <th><?php esc_html_e( '設立', 'sanai-textdomain' ); ?></th>
-        <td><?php esc_html_e( 'YYYY年M月D日', 'sanai-textdomain' ); ?></td>
+        <td><?php esc_html_e( '平成7年12月1日', 'sanai-textdomain' ); ?></td>
+      </tr>
+      <tr>
+        <th><?php esc_html_e( '取締役', 'sanai-textdomain' ); ?></th>
+        <td><?php esc_html_e( '天久 太郎（アメク タロウ）', 'sanai-textdomain' ); ?></td>
       </tr>
       <tr>
         <th><?php esc_html_e( '資本金', 'sanai-textdomain' ); ?></th>
-        <td><?php esc_html_e( 'X,XXX万円', 'sanai-textdomain' ); ?></td>
+        <td><?php esc_html_e( '1,000万円', 'sanai-textdomain' ); ?></td>
       </tr>
       <tr>
-        <th><?php esc_html_e( '免許番号', 'sanai-textdomain' ); ?></th>
-        <td><?php esc_html_e( '神奈川県知事（7）第21620号', 'sanai-textdomain' ); ?></td>
+        <th><?php esc_html_e( '事業内容', 'sanai-textdomain' ); ?></th>
+        <td><?php esc_html_e( '賃貸管理・賃貸仲介・売買仲介・資産コンサルティング・駐車場管理・運営', 'sanai-textdomain' ); ?></td>
       </tr>
       <tr>
         <th><?php esc_html_e( '所在地', 'sanai-textdomain' ); ?></th>
         <td><?php esc_html_e( '横浜市旭区鶴ヶ峰2丁目2-3', 'sanai-textdomain' ); ?></td>
       </tr>
       <tr>
-        <th><?php esc_html_e( '主要取引銀行', 'sanai-textdomain' ); ?></th>
-        <td><?php esc_html_e( '〇〇銀行、△△銀行', 'sanai-textdomain' ); ?></td>
+        <th><?php esc_html_e( '加盟団体', 'sanai-textdomain' ); ?></th>
+        <td><?php esc_html_e( '（公社）全国宅地建物取引業保証協会', 'sanai-textdomain' ); ?></td>
+      </tr>
+      <tr>
+        <th><?php esc_html_e( '取得免許', 'sanai-textdomain' ); ?></th>
+        <td><?php esc_html_e( '宅地建物取引業 神奈川県知事(7) 第021620号', 'sanai-textdomain' ); ?></td>
+      </tr>
+      <tr>
+        <th><?php esc_html_e( '顧問税理士', 'sanai-textdomain' ); ?></th>
+        <td><?php esc_html_e( '高橋典子税理士事務所　高橋典子', 'sanai-textdomain' ); ?></td>
       </tr>
     </tbody>
   </table>
 </section>
+
+
+
+
+
+
 
